eee63e5c13bcc773d2922729a823747a1cadc140
AlanSeyfir <alan.garcia.nunez@gmail.com>
2022-08-04 11:21:27 -0500

    Change the any now working

--- src/app/pipes/search-country.pipe.ts
+++ src/app/pipes/search-country.pipe.ts
@@ -1,8 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-interface Country {
-  name: string;
-  officialName: string;
+interface ICountry {
+  name: {
+    official: string;
+  };
+  // name: Record<string, Item>;
   capital: string | undefined;
   region: string;
   languages: string[] | undefined;
@@ -10,14 +12,17 @@ interface Country {
   flag: string;
 }
 
+interface Item {
+  // name: number;
+  official: number;
+}
+
 @Pipe({
   name: 'searchCountry'
 })
 export class SearchCountryPipe implements PipeTransform {
 
-
-
-  transform(countryArray: Country[], countryName: string): any[] {
+  transform(countryArray: ICountry[], countryName: string): any[] {
     //I think I need to add the return here before countryArr.filter
     return countryArray.filter((country) => (country.name.official as string).toLowerCase().includes(countryName.toLowerCase()));
   };
